530c8a61bc905aa3399862feaa2499dc57f1fc4d
Mario Izquierdo <tothemario@gmail.com>
2012-08-20 01:00:44 -0700

    Complete specs and solve bugs

--- jquery.serializeJSON.js
+++ jquery.serializeJSON.js
@@ -1,76 +1,86 @@
 (function($) {
 
-    var isObject = function(obj) {
-        return obj === Object(obj);
-    };
+  $.fn.serializeJSON = function() {
 
-    // Access the object in a deep key and assigns the value
-    // deepSet(obj, ['foo'], v)                //=> obj['foo'] = v
-    // deepSet(obj, ['foo', 'inn'], v)         //=> obj['foo']['inn'] = v // Create the inner obj['foo'] object, if needed
-    // deepSet(obj, ['foo', 'inn', 'inn'], v)  //=> obj['foo']['inn']['inn'] = v
-    // deepSet(obj, ['0'], v)                  //=> obj[0] = v // obj may be an Array
-    // deepSet(obj, [''], v)                   //=> obj.push(v) // assume obj as array, and add a new value to the end
-    // deepSet(obj, ['arr', '0'], v)           //=> obj['arr']['0'] = v // obj['arr'] is created as Array if needed
-    // deepSet(obj, ['arr', ''], v)            //=> obj['arr'].push(v)
-    // deepSet(obj, ['foo', 'arr', '0'], v)    //=> obj['foo']['arr'][0] = v // obj['foo'] is created as object and obj['foo']['arr'] as a Array, if needed
-    // deepSet(obj, ['arr', '0', 'foo'], v)    //=> obj['arr']['0']['foo'] = v // obj['foo'] is created as object and obj['foo']['arr'] as a Array and obj['foo']['arr'][0] as object, if needed
-    //                               >> obj = []
-    // deepSet(obj, [''], v)         >> obj = [v]
-    // deepSet(obj, ['', 'foo'], v)  >> obj = [v, {foo: v}]
-    // deepSet(obj, ['', 'bar'], v)  >> obj = [v, {foo: v, bar: v}]
-    $._deepSet = function(obj, keys, value) {
-        if (!keys || keys.length === 0) throw new Error("ArgumentError: keys param expected to be an array with least one key");
+    var obj = {};
+    var formAsArray = this.serializeArray();
 
-        var key = keys[0];
-        var next = keys[1];
-        if (next) {
-            var tail = keys.slice(1);
-            var defaultIfNotDefined = (next === '' || !isNaN(parseInt('4', 10))) ? [] : {}; // Array or Object depending on next key
-            if (key === '') { // Empty key with more next keys means to merge keys in the object element
-                var last = obj[obj.length - 1];
-                if (isObject(last)) {
-                    key = last;
-                } else { // if the array does not have an object as last element, create one
-                    obj.push({});
-                    key = last + 1;
-                }
-            }
-            var innerObj = obj[key] || (obj[key] = defaultIfNotDefined);
-            $._deepSet(innerObj, tail, value); // Recursive access the innerObj
-        } else {
-            if (key === '') {
-                obj.push(value);
-            } else {
-                obj[key] = value;
-            }
+    $.each(formAsArray, function(i, input) {
+      var name = input.name;
+      var value = input.value;
+
+      // Split the input name in programatically readable keys
+      // name = "foo"              => keys = ['foo']
+      // name = "[foo]"            => keys = ['foo']
+      // name = "foo[inn][bar]"    => keys = ['foo', 'inn', 'bar']
+      // name = "foo[inn][arr][0]" => keys = ['foo', 'inn', 'arr', '0']
+      // name = "arr[][val]"       => keys = ['arr', '', 'val']
+      var keys = $.map(name.split('['), function(key) {
+        var last = key[key.length - 1];
+        return last == ']' ? key.substring(0, key.length - 1) : key;
+      });
+      if (keys[0] === '') keys.shift(); // "[foo][inn]" should be same as "foo[inn]"
+
+      // Set value in the object using the keys
+      $._deepSet(obj, keys, value);
+    });
+    return obj;
+  };
+
+  /**
+  Access the object in a deep key and assigns the value:
+
+  // Examples:
+  deepSet(obj, ['foo'], v)                //=> obj['foo'] = v
+  deepSet(obj, ['foo', 'inn'], v)         //=> obj['foo']['inn'] = v // Create the inner obj['foo'] object, if needed
+  deepSet(obj, ['foo', 'inn', 'inn'], v)  //=> obj['foo']['inn']['inn'] = v
+  deepSet(obj, ['0'], v)                  //=> obj[0] = v // obj may be an Array
+  deepSet(obj, [''], v)                   //=> obj.push(v) // assume obj as array, and add a new value to the end
+  deepSet(obj, ['arr', '0'], v)           //=> obj['arr']['0'] = v // obj['arr'] is created as Array if needed
+  deepSet(obj, ['arr', ''], v)            //=> obj['arr'].push(v)
+  deepSet(obj, ['foo', 'arr', '0'], v)    //=> obj['foo']['arr'][0] = v // obj['foo'] is created as object and obj['foo']['arr'] as a Array, if needed
+  deepSet(obj, ['arr', '0', 'foo'], v)    //=> obj['arr']['0']['foo'] = v // obj['foo'] is created as object and obj['foo']['arr'] as a Array and obj['foo']['arr'][0] as object, if needed
+
+  // Complex example with array empty index,
+  // it creates a new element, unless there is a nested non repeated key, so it assigns to the last element object:
+  var arr = []
+  deepSet(arr, [''], v)                   //=> arr === [v]
+  deepSet(arr, ['', 'foo'], v)            //=> arr === [v, {foo: v}]
+  deepSet(arr, ['', 'bar'], v)            //=> arr === [v, {foo: v, bar: v}]
+  deepSet(arr, ['', 'bar'], v)            //=> arr === [v, {foo: v, bar: v}, {bar: v}]
+  */
+  $._deepSet = function(obj, keys, value) {
+    if (!keys || keys.length === 0) throw new Error("ArgumentError: keys param expected to be an array with least one key");
+
+    var key = keys[0];
+    var next = keys[1];
+    if (typeof next !== "undefined" && next !== null) {
+      var tail = keys.slice(1);
+      var defaultIfNotDefined = (next === '' || !isNaN(parseInt('4', 10))) ? [] : {}; // Array or Object depending on next key
+      if (key === '') { // Empty key with => merge keys in the object element
+        var lastKey = obj.length - 1;
+        var lastElement = obj[obj.length - 1];
+        if (isObject(lastElement) && !lastElement[next] ) { // if next key is a new attribute in the last object element then set the new value in there
+          key = lastKey;
+        } else { // if the array does not have an object as last element, create one
+          obj.push({});
+          key = lastKey + 1;
         }
+      }
+      obj[key] || (obj[key] = defaultIfNotDefined); // obj[key] ||= defaultIfNotDefined
+      $._deepSet(obj[key], tail, value); // Recursive access the inner Object
+    } else {
+      if (key === '') {
+        obj.push(value);
+      } else {
+        obj[key] = value;
+      }
     }
+  };
 
+  // Auxiliar function to check if a variable is an Object
+  var isObject = function(obj) {
+    return obj === Object(obj);
+  };
 
-    $.fn.serializeJSON = function() {
-
-        var obj = {};
-        var formAsArray = this.serializeArray();
-
-        $.each(formAsArray, function(i, input) {
-            var name = input.name;
-            var value = input.value;
-
-            // Split the input name in programatically readable keys
-            // name = "foo"              => keys = ['foo']
-            // name = "[foo]"            => keys = ['foo']
-            // name = "foo[inn][bar]"    => keys = ['foo', 'inn', 'bar']
-            // name = "foo[inn][arr][0]" => keys = ['foo', 'inn', 'arr', '0']
-            // name = "arr[][val]"       => keys = ['arr', '', 'val']
-            var keys = $.map(name.split('['), function(key) {
-                var last = key[key.length - 1];
-                return last == ']' ? key.substring(0, key.length - 1) : key;
-            });
-            if (keys[0] === '') keys.shift(); // "[foo][inn]" should be same as "foo[inn]"
-
-            // Set value in the object using the keys
-            $._deepSet(obj, keys, value);
-        });
-        return obj;
-    };
 })(jQuery);
--- spec/spec.js
+++ spec/spec.js
@@ -1,28 +1,142 @@
-describe("deepSet", function() {
-  var deepSet = $._deepSet;
+describe("$.serializeJSON", function () {
   var obj;
 
-  beforeEach(function() {
-    obj = {};
+  beforeEach(function () {
+    obj = $('#user-form').serializeJSON();
   });
 
-  it("['foo']", function() {
-    deepSet(obj, ['foo'], 'val');
-    expect(obj['foo']).toEqual('val');
-  });
+  it("should serialize the form inputs", function () {
+    expect(obj).not.toBeNull();
+    expect(obj).toEqual({
+      fullName: "Mario Izquierdo",
 
+      address: {
+        city: "San Francisco",
+        state: {
+          name: "California",
+          abbr: "CA"
+        }
+      },
+
+      jobbies: ["code", "climbing"],
+
+      projects: [
+        { name: "serializeJSON", language: "javascript" },
+        { name: "bettertabs",    language: "ruby" }
+      ]
+    });
+  });
 });
 
-describe("$.serializeJSON", function() {
-  var obj;
+// $._deepSet is used to assign complex keys like "address[state][abbr]" to an object
+describe("deepSet", function () {
+  var deepSet = $._deepSet;
+  var arr, obj, v, v2;
 
-  beforeEach(function() {
-    obj = $('#myform').serializeJSON();
+  beforeEach(function () {
+    obj = {};
+    arr = [];
+    v = 'val';
+    v2 = 'newval'
   });
 
-  it("should apply to a form", function() {
-    expect(obj).not.toBeNull();
-    expect(obj['foo']).toEqual('foo');
+  it("simple attr ['foo']", function () {
+    deepSet(obj, ['foo'], v);
+    expect(obj).toEqual({foo: v});
+  });
+
+  it("simple attr ['foo'] twice should set the last value", function () {
+    deepSet(obj, ['foo'], v);
+    deepSet(obj, ['foo'], v2);
+    expect(obj).toEqual({foo: v2});
+  });
+
+  it("nested attr ['inn', 'foo']", function () {
+    deepSet(obj, ['inn', 'foo'], v);
+    expect(obj).toEqual({inn: {foo: v}});
+  });
+
+  it("nested attr ['inn', 'foo'] twice should set the last value", function () {
+    deepSet(obj, ['inn', 'foo'], v);
+    deepSet(obj, ['inn', 'foo'], v2);
+    expect(obj).toEqual({inn: {foo: v2}});
+  });
+
+  it("multiple assign attr ['foo'] and ['inn', 'foo']", function () {
+    deepSet(obj, ['foo'], v);
+    deepSet(obj, ['inn', 'foo'], v);
+    expect(obj).toEqual({foo: v, inn: {foo: v}});
+  });
+
+  it("very nested attr ['inn', 'inn', 'inn', 'foo']", function () {
+    deepSet(obj, ['inn', 'inn', 'inn', 'foo'], v);
+    expect(obj).toEqual({inn: {inn: {inn: {foo: v}}}});
+  });
+
+  it("simple array ['0']", function () {
+    deepSet(arr, ['0'], v);
+    expect(arr).toEqual([v]);
+  });
+
+  it("nested simple array ['arr', '0']", function () {
+    deepSet(obj, ['arr', '0'], v);
+    expect(obj).toEqual({'arr': [v]});
+  });
+
+  it("nested simple array multiple values", function () {
+    deepSet(obj, ['arr', '1'], v2);
+    deepSet(obj, ['arr', '0'], v);
+    expect(obj).toEqual({'arr': [v, v2]});
+  });
+
+  it("array assign with empty index should push the element", function () {
+    deepSet(arr, [''], 1);
+    deepSet(arr, [''], 2);
+    deepSet(arr, [''], 3);
+    expect(arr).toEqual([1,2,3]);
+  });
+
+  it("nested array assign with empty index should push the element", function () {
+    deepSet(obj, ['arr', ''], 1);
+    deepSet(obj, ['arr', ''], 2);
+    deepSet(obj, ['arr', ''], 3);
+    expect(obj).toEqual({arr: [1,2,3]});
+  });
+
+  it("nested object as array element ['arr', '0', 'foo']", function () {
+    deepSet(obj, ['arr', '0', 'foo'], v);
+    expect(obj).toEqual({arr: [{foo: v}]});
+  });
+
+  it("array of objects", function (){
+    deepSet(arr, ['0', 'foo'], v);
+    deepSet(arr, ['0', 'bar'], v);
+    deepSet(arr, ['1', 'foo'], v2);
+    deepSet(arr, ['1', 'bar'], v2);
+    expect(arr).toEqual([{foo: v, bar: v}, {foo: v2, bar: v2}]);
+  });
+
+  it("array push with empty index, if repeat same object element key then it creates a new element", function () {
+    deepSet(arr, [''], v);        //=> arr === [v]
+    deepSet(arr, ['', 'foo'], v); //=> arr === [v, {foo: v}]
+    deepSet(arr, ['', 'bar'], v); //=> arr === [v, {foo: v, bar: v}]
+    deepSet(arr, ['', 'bar'], v); //=> arr === [v, {foo: v, bar: v}, {bar: v}]
+    expect(arr).toEqual([v, {foo: v, bar: v}, {bar: v}]);
+  });
+
+  it("should set all different nested values", function () {
+    deepSet(obj, ['foo'], v);
+    deepSet(obj, ['inn', 'foo'], v);
+    deepSet(obj, ['inn', 'arr', '0'], v);
+    deepSet(obj, ['inn', 'arr', '1'], v2);
+    deepSet(obj, ['inn', 'arr', '2', 'foo'], v);
+    deepSet(obj, ['inn', 'arr', '2', 'bar'], v);
+    deepSet(obj, ['inn', 'arr', ''], v);
+    deepSet(obj, ['inn', 'arr', ''], v2);
+    deepSet(obj, ['inn', 'arr', '', 'foo'], v2);
+    deepSet(obj, ['inn', 'arr', '', 'bar'], v2);
+    deepSet(obj, ['inn', 'arr', '2', 'inn', 'foo'], v);
+    expect(obj).toEqual({foo: v, inn: {foo: v, arr: [v, v2, {foo: v, bar: v, inn: {foo: v}}, v, v2, {foo: v2, bar: v2}]}})
   });
 
 });
